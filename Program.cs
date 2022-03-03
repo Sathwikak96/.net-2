d6d17b92b208a0a25c8f503643bba6027f27e336
Sathwikak96 <97940151+Sathwikak96@users.noreply.github.com>
2022-03-03 11:29:26 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.IO;
+using System.Text;
+
+namespace IncludeHelp
+{
+    class Test
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("sizeof(int): {0}", sizeof(int));
+            Console.WriteLine("sizeof(float): {0}", sizeof(float));
+            Console.WriteLine("sizeof(char): {0}", sizeof(char));
+            Console.WriteLine("sizeof(double): {0}", sizeof(double));
+            Console.WriteLine("sizeof(bool): {0}", sizeof(bool));
+            Console.ReadLine();
+        }
+    }
+}
