b801f3b5d72f327223b27db988f05dc633b6ba22
Sathwikak96 <97940151+Sathwikak96@users.noreply.github.com>
2022-03-03 11:40:02 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+class CubeRoot
+{
+    static int Main()
+    {
+        int number = 0;
+        double cubeRoot = 0;
+
+        Console.Write("Enter the value of number: ");
+        number = Convert.ToInt32(Console.ReadLine());
+
+        cubeRoot = Math.Ceiling(Math.Pow(number, (double)1 / 3));
+        Console.WriteLine("Cube Root is : " + cubeRoot);
+
+        return 0;
+    }
+}
