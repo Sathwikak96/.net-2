25a98a28a0531fb75ac2ccbe420aa358c9cace08
Sathwikak96 <97940151+Sathwikak96@users.noreply.github.com>
2022-03-03 11:40:49 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+class SquareRoot
+{
+    static int Main()
+    {
+        int number = 0;
+        double squareRoot = 0;
+
+        Console.Write("Enter the value of number: ");
+        number = Convert.ToInt32(Console.ReadLine());
+
+        squareRoot = Math.Ceiling(Math.Pow(number, (double)1 / 2));
+        Console.WriteLine("Square Root is : " + squareRoot);
+
+        return 0;
+    }
+}
