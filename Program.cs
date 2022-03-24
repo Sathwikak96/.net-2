fa5a84e154e5b0c47c47aa59bb16276da7381e7f
Sathwikak96 <97940151+Sathwikak96@users.noreply.github.com>
2022-03-24 12:09:29 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Exercises
+{
+    class Hello
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
